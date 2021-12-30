2edab043eac4dfc5cd9c8d90687d83e9787f7cbb
Lee <leon.lee2050@gmail.com>
2021-12-29 21:41:45 -0500

    finished

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	hello_world_pb_v1 "github.com/Xin2050/grpc_gateway_demo/proto/gen/hello_world/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net"
+	"net/http"
 )
 
 type server struct {
@@ -25,11 +28,49 @@ func main() {
 		log.Fatalln("Failed to listen:", err)
 	}
 
-	// Create a gRPC server object
+	// Create a gRPC server object gRPC server object
 	s := grpc.NewServer()
 	// Attach the Greeter service to the server
 	hello_world_pb_v1.RegisterGreeterServer(s, &server{})
 	// Serve gRPC Server
 	log.Println("Serving gRPC on 0.0.0.0:8080")
-	log.Fatal(s.Serve(lis))
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+	defer conn.Close()
+	client := hello_world_pb_v1.NewGreeterClient(conn)
+	result, err := client.SayHello(context.Background(), &hello_world_pb_v1.HelloRequest{
+		Name: "Leon",
+	})
+	if err != nil {
+		log.Fatalf("Fail to create blog %v", err)
+		return
+	}
+	log.Println(result.Message)
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = hello_world_pb_v1.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
 }
